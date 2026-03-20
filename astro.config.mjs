2b66c4dbaca9c0f24290592ea95ffa4bd3ee2791
Martin <ouedraogomartin12@yahoo.fr>
2026-03-20 10:47:29 +0100

    fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ton-username.github.io',
-  base: '/nom-du-repo',
+  base: '/Portfolio',
 });
 
