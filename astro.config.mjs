b1080f5df0b4bc5f09ca2d8ee074ffc9c81411ca
Martin <ouedraogomartin12@yahoo.fr>
2026-03-20 11:09:02 +0100

    Changement de config github page vers vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ton-username.github.io',
-  base: '/Portfolio',
+  site: 'https://ouedraogo-martin.vercel.app',
 });
 
